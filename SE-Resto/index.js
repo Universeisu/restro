ea86b196c29f4c29d5497bd0c36cf88d5a67fd74
Universeisu <111554684+Universeisu@users.noreply.github.com>
2023-02-23 16:45:25 +0700

    Add files via upload

--- SE-Resto/index.js
+++ SE-Resto/index.js
@@ -18,3 +18,49 @@ close_icon.onclick = () =>{
     let search_form = document.querySelector("#search-form");
     search_form.classList.toggle('active')
 }
+
+//Swiper function
+var swiper = new Swiper(".home-slider",{
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+        delay:3500,
+        disableOnInteraction:false,
+    },
+    pagination :{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    
+});
+
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+        delay:3500,
+        disableOnInteraction:false,
+    },
+    pagination :{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    loop:true,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+
+    }
+    
+});
